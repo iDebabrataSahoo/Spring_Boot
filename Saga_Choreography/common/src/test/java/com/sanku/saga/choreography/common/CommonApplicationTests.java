0a1ab5d6fc270905c935328aedd1372a7ea187f0
Debabrata Sahoo <i.debabratasahoo@gmail.com>
2026-09-14 18:27:34 +0530

    basic structure added

---
+++ Saga_Choreography/common/src/test/java/com/sanku/saga/choreography/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanku.saga.choreography.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
